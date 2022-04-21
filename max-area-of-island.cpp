fc5fa6a48192f58fba8e15f896bad3260ed47963
Yue Jiao <cleamoon@hotmail.com>
2022-04-22 00:34:40 +0200

    Upload solution(s)

--- max-area-of-island.cpp
+++ max-area-of-island.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
-        int maxSize = 0;
+    int dfs(vector<vector<int>>& grid, int x, int y) {
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == 0) {
+            return 0;
+        }
+        grid[x][y] = 0;
+        int sum = 1;
+        sum += dfs(grid, x - 1, y);
+        sum += dfs(grid, x + 1, y);
+        sum += dfs(grid, x, y - 1);
+        sum += dfs(grid, x, y + 1);
+        return sum;
+    }
 
-        for (auto i = 0; i < grid.size(); i++) {
-            for (auto j = 0; j < grid[0].size(); j++) {
-                if (!visited[i][j] && grid[i][j]) {
-                    visited[i][j] = true;
-                    int size = 0;
-                    queue<vector<int>> q;
-                    q.push(vector<int>{i, j});
-                    while(!q.empty()) {
-                        size ++;
-                        int x = q.front()[0];
-                        int y = q.front()[1];
-                        q.pop();
-                        if (x != 0 && grid[x-1][y] && !visited[x-1][y]) {
-                            q.push(vector<int>{x-1, y});
-                            visited[x-1][y] = true;
-                        }
-                        if (x != grid.size()-1 && grid[x+1][y] && !visited[x+1][y]) {
-                            q.push(vector<int>{x+1, y});
-                            visited[x+1][y] = true;
-                        }
-                        if (y != 0 && grid[x][y-1] && !visited[x][y-1]) {
-                            q.push(vector<int>{x, y-1});
-                            visited[x][y-1] = true;
-                        }
-                        if (y != grid[0].size()-1 && grid[x][y+1] && !visited[x][y+1]) {
-                            q.push(vector<int>{x, y+1});
-                            visited[x][y+1] = true;
-                        }
-                    }
-                    if (size > maxSize) {
-                        maxSize = size;
-                    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxarea = 0;
+        for(int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j]) {
+                    int area = dfs(grid, i, j);
+                    maxarea = maxarea > area ? maxarea : area;
                 }
             }
         }
-        return maxSize;
+        return maxarea;
     }
+
 };
